a886fc2ffd36dfcfdd05ce4d5292fb57d2d501da
loganpowell <loganp@tepper.cmu.edu>
2017-07-12 11:58:44 -0400

    adding pages and component structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
-let Fridge = () => {
-  return (<div>Fridge</div>)
+class Header extends Component {
+  render() { return <div>HEADER</div> }
 }
+class Footer extends Component {
+  render() { return <div>FOOTER</div> }
+}
+class Fridge extends Component {
+  render() { return <div>This is the fridge</div> }
+}
+class Recipes extends Component {
+  render() { return <div>These are the recipes</div> }
+}
+class MyRecipes extends Component {
+  render() { return <div>These are MY SAVED recipes</div> }
+}
+class Cart extends Component {
+  render() { return <div>This is the Shopping Cart</div> }
+}
+class SignIn extends Component {
+  render() { return <div>Sign in HERE</div> }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        Ola
-        <Route path="/fridge" compontent={Fridge} />
+        <Header />
+        <Route path="/fridge" component={Fridge} />
+        <Route path="/recipes" component={Recipes} />
+        <Route path="/my-recipes" component={MyRecipes} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/sign-in" component={SignIn} />
+        <Footer />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -5,44 +5,16 @@ import { BrowserRouter, Route } from 'react-router-dom'
 // import { createStore, applyMiddleware } from 'redux'
 import registerServiceWorker from './registerServiceWorker';
 
-// import App from './App';
+import App from './App';
 
 // const createStoreWithMiddleare = applyMiddleware()(createStore)
 
-class Header extends Component {
-  render() { return <div>HEADER</div> }
-}
-class Footer extends Component {
-  render() { return <div>FOOTER</div> }
-}
-class Fridge extends Component {
-  render() { return <div>This is the fridge</div> }
-}
-class Recipes extends Component {
-  render() { return <div>These are the recipes</div> }
-}
-class MyRecipes extends Component {
-  render() { return <div>These are MY SAVED recipes</div> }
-}
-class Cart extends Component {
-  render() { return <div>This is the Shopping Cart</div> }
-}
-class SignIn extends Component {
-  render() { return <div>Sign in HERE</div> }
-}
+
 
 ReactDOM.render((
   // <Provider store={createStoreWithMiddleare(reducers)}>
     <BrowserRouter>
-      <div>
-        <Header />
-        <Route path="/fridge" component={Fridge} />
-        <Route path="/recipes" component={Recipes} />
-        <Route path="/my-recipes" component={MyRecipes} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/sign-in" component={SignIn} />
-        <Footer />
-      </div>
+      <App />
     </BrowserRouter>
   // </Provider>
 ), document.getElementById('root'))
