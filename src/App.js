136d7b687f6da4330e10cd2cf07f3de824e6dc4b
loganpowell <loganp@tepper.cmu.edu>
2017-07-11 20:48:58 -0400

    getting React Router figured out

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+let Fridge = () => {
+  return (<div>Fridge</div>)
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        Ola
+        <Route path="/fridge" compontent={Fridge} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
