d2ae6786e3733c519b6586f6ca47475a5684dd4e
Liang Cheng <liangcheng6@gmail.com>
2017-07-16 11:54:27 -0400

    update server

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,17 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//TODO: fix the warning here 
-mongoose.connect(process.env.MONGODB_URI);
+//TODO: fix the deprecation warning here
+let db = mongoose.connect(process.env.MONGODB_URI);
+
+// Show any mongoose errors if the connection is not made for some reason.
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
+});
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post('/users', (req, res) => {
         res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
-    })
+    });
 });
 
 
