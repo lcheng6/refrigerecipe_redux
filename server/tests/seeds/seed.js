e3eb969624f07379a5bcf19c59df8109e525a566
Liang Cheng <liangcheng6@gmail.com>
2017-07-16 02:25:18 -0400

    first stage of creating users.

---
+++ server/tests/seeds/seed.js
@@ -0,0 +1,34 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {User} = require('./../../models/users_model');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [{
+    _id: userOneId,
+    email: 'andrew@example.com',
+    password: 'userOnePass',
+    mobilenumber: "(571)243-0741",
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+    }]
+}, {
+    _id: userTwoId,
+    email: 'jen@example.com',
+    password: 'userTwoPass',
+    mobilenumber: "(571)243-0741"
+}];
+
+
+const populateUsers = (done) => {
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+
+        return Promise.all([userOne, userTwo])
+    }).then(() => done());
+};
+
+module.exports = {users, populateUsers};
