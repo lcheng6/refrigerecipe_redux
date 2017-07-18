25284157590faf10ed2c43c6a2eaf3e31ca1930e
Liang Cheng <liangcheng6@gmail.com>
2017-07-18 02:58:26 -0400

    recipes ground work

---
+++ server/models/recipes_model.js
@@ -0,0 +1,28 @@
+'use strict';
+require('../config/config');
+
+const mongoose = require('mongoose');
+const validator = require('validator');
+const _ = require('lodash');
+
+//insert a recipe schema.
+var RecipeSchema = new mongoose.Schema({
+  fridge_name: {
+    type:String,
+    required: true
+  },
+  //this id is from the spoonacular API.
+  //it won't be the Mongoose _id.
+  id: {
+    type: Number,
+    required: true
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
+});
+
+var Recipe = mongoose.model('Recipe', FridgeSchema);
+
+module.exports = {Recipe};
