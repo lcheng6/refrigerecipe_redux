f71ba432408b86d77d4f4c173b1cdebba86954db
Liang Cheng <liangcheng6@gmail.com>
2017-07-30 10:38:55 -0400

    update some UI components

--- src/views/components/recipe-detail-modal/recipe-detail-modal.js
+++ src/views/components/recipe-detail-modal/recipe-detail-modal.js
@@ -18,20 +18,17 @@ export class RecipeDetailModal extends Component {
     });
   }
 
-  renderMissingIngredients() {
-    const recipe = this.props.recipeDetail;
 
-  }
 
   renderHeaderBar() {
-    const recipe = this.props.recipeDetail;
+    const recipeDetail = this.props.recipeDetail;
     return (
       <div className="ui horizontal segments">
         <div className="ui segment">
           <div className="ui tiny statistic">
             <div className="value">
               <i className="spoon icon"/>
-              {!undefined != recipe.servings? recipe.servings : 2}
+              {!undefined !== recipeDetail.servings? recipeDetail.servings : 2}
             </div>
             <div className="label">
               Servings
@@ -42,7 +39,7 @@ export class RecipeDetailModal extends Component {
           <div className="ui tiny statistic">
             <div className="value">
               <i className="heartbeat icon"/>
-              {undefined != recipe.nutrition.nutrients[0].amount? recipe.nutrition.nutrients[0].amount: 200}
+              {undefined !== recipeDetail.nutrition.nutrients[0].amount? recipeDetail.nutrition.nutrients[0].amount: 200}
             </div>
             <div className="label">
               Calories
@@ -53,7 +50,7 @@ export class RecipeDetailModal extends Component {
           <div className="ui tiny statistic">
             <div className="value">
               <i className="dashboard icon"/>
-              {undefined != recipe.readyInMinutes? recipe.readyInMinutes: 45}
+              {undefined !== recipeDetail.readyInMinutes? recipeDetail.readyInMinutes: 45}
             </div>
             <div className="label">
               Prep time
@@ -64,7 +61,7 @@ export class RecipeDetailModal extends Component {
           <div className="ui tiny statistic">
             <div className="value">
               <i className="wait icon"/>
-              {undefined != recipe.cookingMinutes ? recipe.cookingMinutes: 25}
+              {undefined !== recipeDetail.cookingMinutes ? recipeDetail.cookingMinutes: 25}
             </div>
             <div className="label">
               Cook time
@@ -76,50 +73,71 @@ export class RecipeDetailModal extends Component {
 
   }
 
+  renderRecipeImage() {
+    const recipeDetail = this.props.recipeDetail;
+    return (
+      <div className="ui segment">
+        <div className="ui image">
+          <img className="ui fluid image"
+               src={recipeDetail.image}/>
+        </div>
+      </div>
+    );
+  }
+  renderMissingIngredients() {
+    const recipeDetail = this.props.recipeDetail;
+    // const recipeSummary = this.props.recipeSummary;
+
+    return (
+      <div className="ui segment">
+        <div className="meta">
+          <span>MISSING INGREDIENTS</span>
+        </div>
+        <div className="ui large list">
+          <div className="item">cream cheese</div>
+          <div className="item">fillo shells</div>
+          <div className="item">ground beef round</div>
+          <div className="item">onion soup mix</div>
+          <div className="item">part skim mozzarella cheese</div>
+          <div className="item">part-skim mozzarella cheese</div>
+          <div className="item">pepper sauce</div>
+          <div className="item">quick cooking oats</div>
+          <div className="item">roma tomatoes</div>
+          <div className="item">tomato sauce</div>
+        </div>
+      </div>
+    );
+  }
   renderUsedIngredients() {
-    const recipe = this.props.recipeDetail;
+    const recipeDetail = this.props.recipeDetail;
+    // const recipeSummary = this.props.recipeSummary;
 
   }
 
   renderAvailableInstructions() {
-    const recipe = this.props.recipeDetail;
+    const recipeDetail = this.props.recipeDetail;
 
   }
 
+  renderAvailableInstructions() {
+    const recipeDetail = this.props.recipeDetail;
+  }
+
   render() {
-    const recipe = this.props.recipeDetail;
+    const recipeDetail = this.props.recipeDetail;
     return (
       <div>
         <Button color="danger" onClick={this.toggle}>Recipe Detail Modal</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>{recipe.title}</ModalHeader>
+          <ModalHeader toggle={this.toggle}>{recipeDetail.title}</ModalHeader>
           <ModalBody>
             {this.renderHeaderBar()}
 
             <div className="ui horizontal segments">
-              <div className="ui segment">
-                <div className="ui image">
-                  <img className="ui fluid image"
-                       src="https://spoonacular.com/recipeImages/Touchdown-Mini-Meatloaf-and-Buffalo-Chicken-Bites-595511.jpg"/>
-                </div>
-              </div>
-              <div className="ui segment">
-                <div className="meta">
-                  <span>MISSING INGREDIENTS</span>
-                </div>
-                <div className="ui large list">
-                  <div className="item">cream cheese</div>
-                  <div className="item">fillo shells</div>
-                  <div className="item">ground beef round</div>
-                  <div className="item">onion soup mix</div>
-                  <div className="item">part skim mozzarella cheese</div>
-                  <div className="item">part-skim mozzarella cheese</div>
-                  <div className="item">pepper sauce</div>
-                  <div className="item">quick cooking oats</div>
-                  <div className="item">roma tomatoes</div>
-                  <div className="item">tomato sauce</div>
-                </div>
-              </div>
+              {this.renderRecipeImage()}
+              {this.renderMissingIngredients()}
+
+
               <div className="ui segment">
                 <div className="meta">
                   <span>USED INGREDIENTS</span>
