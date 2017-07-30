e3d0e1acd4239f04d93201a651963e96ba703ea5
Liang Cheng <liangcheng6@gmail.com>
2017-07-30 11:08:38 -0400

    making some progress, fuuuuuuucccccckkkkkkkkk yeah!

--- src/core/api/getRecipesIntroAPI.js
+++ src/core/api/getRecipesIntroAPI.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const API_KEY = 'G33IWA9BzKmshCOQoLZe3QV4LKnup1tNXmdjsnwCZVLOryqB9h';
-const ROOT_URL = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&limitLicense=false&number=50&ranking=1&ingredients=`
+const ROOT_URL = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients';
 
 // redux-promise-middleware requires it get a promise,
 // so we create one with the api call (also a promise)
@@ -11,10 +11,18 @@ const getRecipesByIngredients = (ingredients) => {
   return new Promise((resolve, reject) => {
     axios({
       method: 'get',
-      url: ROOT_URL + ingredients + API_KEY,
+      url: ROOT_URL,
       headers: {
         "X-Mashape-Key": API_KEY,
         "Accept": "application/json",
+      },
+      params: {
+        "fillIngredients": "true",
+        // "ingredients": ["apples","flour","sugar"].join(","),
+        "ingredients": ingredients,
+        "limitLicense": "false",
+        "number": "50",
+        "ranking": "1"
       }
     })
     .then((response) => {
