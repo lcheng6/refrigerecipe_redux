95b4dab07aa22d9671a75edc71fd16285dceb3ca
Liang Cheng <liangcheng6@gmail.com>
2017-07-29 20:10:39 -0400

    improving modal bit by bit. Fuck this stupid fucker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
 import './semantic/dist/semantic.css';
+import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
