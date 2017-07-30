21a65078e8c6514cb7db4639ae7b20d20108e39d
Liang Cheng <liangcheng6@gmail.com>
2017-07-30 11:03:41 -0400

    additional improvement in recipe detail

--- src/views/containers/recipe-detail-modal/index.js
+++ src/views/containers/recipe-detail-modal/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import RecipeDetailModal from 'src/views/components/recipe-detail-modal';
 
@@ -12,14 +13,21 @@ export class RecipeDetailModalContainer extends Component {
 
   render() {
     if(this.props.currentRecipeId) {
+      let recipeSummaryIndex = _.findIndex(this.props.recipeSummaries,
+        function(elem) {
+          return elem.id === this.props.currentRecipeId;
+        }.bind(this)
+      );
+
       return (
         <RecipeDetailModal
           title="Recipe Detail"
           recipeDetail={this.props.recipeDetails[this.props.currentRecipeId]}
+          recipeSummary={this.props.recipeSummaries[recipeSummaryIndex]}
         />
       );
     }else {
-      return (<div/>);
+      return null;
     }
   }
 }
@@ -28,6 +36,7 @@ function mapStateToProps(state) {
   return {
     recipeDetails:state.recipe_details.recipeDetails,
     currentRecipeId:state.recipe_details.currentRecipeId,
+    recipeSummaries:state.intro_recipes.recipes,
   };
 }
 export default connect(mapStateToProps)(RecipeDetailModalContainer);
